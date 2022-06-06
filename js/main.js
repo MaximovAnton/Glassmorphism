afe5b69623c40dc915b806eb4660092c0166f0f8
Anton <maximov.antonn@gmail.com>
2022-06-06 21:39:35 +0300

    +

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 VanillaTilt.init(document.querySelectorAll(".box"), {
     max: 25,
-    speed: 400,
-    easing:"cubic-bezier(.03,.98,.52,.99)",
-    perspective:500,
+    speed: 300,
+    easing:"cubic-bezier(.83,.98,.52,.99)",
+    perspective:800,
     transition:true
 });
